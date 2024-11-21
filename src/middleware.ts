1dc0b44f010cd0ff34d3279227ffd8e3c4fa5bd3
jhn02222 <jhn02222@uga.edu>
2024-11-21 14:26:03 -0500

    changed unauthorized page and redirect for unauthorized users

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,12 +10,12 @@ console.log('Token:', token);  // Debug token value
 
   // Define protected and excluded routes
   const protectedRoutes = ['/home', '/edit-item'];
-  const excludedRoutes = ['/login'];
+  const excludedRoutes = ['/login', '/'];
 
   // If the user is not authenticated and trying to access protected routes, redirect to login
   if (!isAuthenticated && protectedRoutes.some(route => request.nextUrl.pathname.startsWith(route))) {
     console.log('User not authenticated, redirecting to login page');
-    return NextResponse.redirect(new URL('/login', request.url));
+    return NextResponse.redirect(new URL('/', request.url));
   }
 
   // Allow access to routes that don't require authentication (like login page)
