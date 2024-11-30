121b7b234183c3e82abf83c337e5f19b5ac3beb0
jhn02222 <jhn02222@uga.edu>
2024-11-30 17:32:50 -0500

    fixed signup enecryption, header issues

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,54 +14,59 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ isAuthorized, logout }) => {
   const router = useRouter();
 
+  // Check if current route is login or signup to hide the header
+  const hideHeader = router.pathname === '/login' || router.pathname === '/signup';
+
   return (
-    <header className="header">
-      <div className="header-left">
-        {isAuthorized && (
-          <>
-            {/* Replace Profile Home button with User Icon */}
-            <Link href="/profile-home">
-              <button className="icon-button profile-button">
-                <FontAwesomeIcon icon={faUser} />
-              </button>
-            </Link>
-            <Link href="/profile">
-              <button className="settings-button">Settings</button>
-            </Link>
-          </>
-        )}
-      </div>
+    !hideHeader && (
+      <header className="header">
+        <div className="header-left">
+          {isAuthorized && (
+            <>
+              {/* Replace Profile Home button with User Icon */}
+              <Link href="/profile-home">
+                <button className="icon-button profile-button">
+                  <FontAwesomeIcon icon={faUser} />
+                </button>
+              </Link>
+              <Link href="/profile">
+                <button className="settings-button">Settings</button>
+              </Link>
+            </>
+          )}
+        </div>
 
-      <div className="header-center">
-        <Image
-          src="/campusConnectLogo.webp"
-          alt="Campus Connect Logo"
-          className="header-logo"
-          width={40}
-          height={40}
-        />
-        <h1>
-          <Link href="/home">Campus Connect</Link>
-        </h1>
-      </div>
+        <div className="header-center">
+          <Image
+            src="/campusConnectLogo.webp"
+            alt="Campus Connect Logo"
+            className="header-logo"
+            width={40}
+            height={40}
+          />
+          <h1>
+            <Link href="/home">Campus Connect</Link>
+          </h1>
+        </div>
 
-      <div className="header-right">
-        {isAuthorized ? (
-          <button className="logout-button" onClick={logout}>
-            Logout
-          </button>
-        ) : (
-          <>
-            <Link href="/login">
-              <button className="header-button">Login</button>
-            </Link>
-            <Link href="/signup">
-              <button className="header-button">Sign Up</button>
-            </Link>
-          </>
-        )}
-      </div>
-    </header>
+        <div className="header-right">
+          {isAuthorized ? (
+            <button className="logout-button" onClick={logout}>
+              Logout
+            </button>
+          ) : (
+            <>
+              <Link href="/login">
+                <button className="header-button">Login</button>
+              </Link>
+              <Link href="/signup">
+                <button className="header-button">Sign Up</button>
+              </Link>
+            </>
+          )}
+        </div>
+      </header>
+    )
   );
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,38 @@
-import React, { useState } from 'react';
+// _app.tsx
+import React, { useState, useEffect } from 'react';
 import { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import Header from '../components/Header/Header';
-import { SessionProvider } from 'next-auth/react'; 
+import { SessionProvider } from 'next-auth/react';
 import '../styles/App.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
-config.autoAddCss = false; 
+
+config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [isAuthorized, setIsAuthorized] = useState(false);
   const router = useRouter();
 
-  // Logout function
+  // Make sure the header doesn't show up right after signup
+  useEffect(() => {
+    if (router.pathname === '/login' || router.pathname === '/signup') {
+      setIsAuthorized(false); // Reset authorization status on signup/login
+    }
+  }, [router.pathname]);
+
   const logout = () => {
     setIsAuthorized(false);
-    router.push('/'); 
+    router.push('/');
   };
 
-  const showHeader = router.pathname !== '/' && isAuthorized;
+  const showHeader = router.pathname !== '/' && isAuthorized; // Hide header on login and signup pages
 
   return (
-    <SessionProvider session={pageProps.session}> 
+    <SessionProvider session={pageProps.session}>
       <div>
         {showHeader && (
-          <Header
-            setIsAuthorized={setIsAuthorized}
-            isAuthorized={isAuthorized}
-            logout={logout}
-          />
+          <Header isAuthorized={isAuthorized} logout={logout} />
         )}
         <Component {...pageProps} setIsAuthorized={setIsAuthorized} isAuthorized={isAuthorized} />
       </div>
